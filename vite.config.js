7283a704e0989f79322b18758b4ba144c472c74f
Novac Pavel <pavel.novac@sens.media>
2025-11-25 01:10:36 +0200

    Fix: Use conditional base path for dev vs production

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/rcahub/',
+  base: process.env.NODE_ENV === 'production' ? '/rcahub/' : '/',
 })
 
 
